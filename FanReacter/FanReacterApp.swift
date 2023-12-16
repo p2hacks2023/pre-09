6a4660e12fe0e1fc8a81e50ce0018ced8af06841
Hikaru <k.h.light@icloud.com>
2023-12-16 12:01:39 +0900

    ソースコードを追加

---
+++ FanReacter/FanReacterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanReacterApp.swift
+//  FanReacter
+//
+//  Created by 古賀耀 on 2023/12/12.
+//
+
+import SwiftUI
+
+@main
+struct FanReacterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
